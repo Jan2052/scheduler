684bb4cb0d987647e8df09fe74eba64379b5ce2d
Janet <Janet_Lam@live.ca>
2022-12-17 17:36:17 -0500

    first commit

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,7 +3,7 @@ import "components/DayListItem.scss";
 import classNames from "classnames";
 
 export default function DayListItem(props) {
-  const buttonClass = classNames("day-list__item", {
+  const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full.": props.spots === 0
   });
@@ -20,8 +20,8 @@ export default function DayListItem(props) {
 
   return (
     <li
-      className={buttonClass}
-      onClick={() => props.setDay(props.name)}
+      className={dayClass}
+      onClick={()=>props.setDay(props.name)}
     >
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
