f08c7dde172b6eb772bf00ebcc058826eaab409b
Janet <Janet_Lam@live.ca>
2023-01-01 17:06:22 -0500

    fixed <DayListItem>. Prints dates in console

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,30 +1,55 @@
 import React from "react";
-import "components/DayListItem.scss";
 import classNames from "classnames";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
-    "day-list__item--full.": props.spots === 0
+    "day-list__item--full": props.spots === 0
   });
 
-  function formatSpots(spots) {
-    if (spots === 0) {
-      return 'no spots remaining'
-    }
-    if (spots === 1) {
-      return '1 spot remaining'
-    }
-    return `${spots} spots remaining`
-  }
-
   return (
-    <li
-      className={dayClass}
-      onClick={()=>props.setDay(props.name)}
-    >
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+      <h3 className="text--light">
+        {!props.spots && <span>no spots remaining</span>}
+        {props.spots === 1 && <span>1 spot remaining</span>}
+        {props.spots > 1 && <span>{props.spots} spots remaining</span>}
+      </h3>
     </li>
   );
 }
+
+// import React from "react";
+// import "components/DayListItem.scss";
+// import classNames from "classnames";
+
+//   function formatSpots(spots) {
+//     if (!spots) {
+//       return 'no spots remaining'
+//     }
+//     if (spots === 1) {
+//       return '1 spot remaining'
+//     }
+//     if (spots > 1) {
+//       return `${spots} spots remaining`
+//     }
+//   }
+
+// export default function DayListItem(props) {
+//   const dayClass = classNames("day-list__item", {
+//     "day-list__item--selected": props.selected,
+//     "day-list__item--full.": props.spots === 0
+//   });
+
+//   return (
+//     <li
+//       className={dayClass}
+//       onClick={()=>props.setDay(props.name)}
+//     >
+//       <h2 className="text--regular">{props.name}</h2>
+//       <h3 className="text--light">{formatSpots(props.spots)}</h3>
+//     </li>
+//   );
+// }
